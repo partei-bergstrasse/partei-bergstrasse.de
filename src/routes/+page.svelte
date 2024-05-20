bbaa6b7d743fa83448b3690670362d2e2fd2d50d
Stefan Buhrmester <buhrmi@gmail.com>
2024-05-20 17:15:30 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,26 +3,10 @@
 </svelte:head>
 
 <div class="wrapper box mt-12">
-  <h1>
-    Herzlich willkommen auf der Webseite des nichtexistierenden Parteiorganes der Partei Die PARTEI Kreisverband Bergstrasse
-  </h1> 
+  
 
   <p>
-    Aller Anfang ist schwer. Doch mit irgendetwas muss man ja anfangen.
-    Darum fangen wir nun damit an, die Frage zu beantworten, die allen auf der Zunge liegt:
-  </p>
-
-  <p class="mt-6">
-    <strong> Warum existiert der KV Bergstrasse nicht?</strong><br>
-    Naja, das ist eigentlich ganz einfach zu erklären. Der 
-    Landesverband Hessen hat den "alten" KV Bergstraße 2021 aufgrund
-    Inaktivität aufgelöst. Nun hat sich eine neue Gruppe von Interessierten zusammengetan.
-    Und jetzt warten wir darauf, dass der Landesverband uns eine
-    Gründungsvertragsvorlage vorlegt. Ohne Gründungsvertragsvorlage
-    kann der KV nicht offiziell gegründet werden. Siehe dazu auch
-    <a href="https://www.die-partei.de/ov-gruendung/" target="_blank">Gründung eines PARTEI-Gebietsverbandes</a>.
-    <br><br>
-    <strong>Daher folgt nun unser Aufruf an den Landesverband Hessen:</strong><br>
+    
     <img src="/mach-mal-hinne-da.png" alt="Jetzt mach mal hinne da">
   </p>
 
